d8f3edc9b30662f185a69764555cc92e9d81bef3
Raqiri <rahmanqodiri@gmail.com>
2025-12-06 22:06:43 +0700

    Update mahasiswa.sql

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2025 at 11:54 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Waktu pembuatan: 03 Des 2025 pada 16.10
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,19 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mahasiswa`
+-- Struktur dari tabel `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `nidn` char(10) NOT NULL,
+  `nama_dosen` varchar(40) DEFAULT NULL,
+  `gender` enum('L','P') DEFAULT NULL,
+  `prodi` enum('TI','TE','SI') DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `dosen`
+--
+
+INSERT INTO `dosen` (`nidn`, `nama_dosen`, `gender`, `prodi`, `email`) VALUES
+('2001013', 'Dr. Lisa Mariana', 'P', 'SI', 'lisa.mariana@universitas.edu'),
+('2001014', 'Prof. Martin S.', 'L', 'TI', 'martin.s@universitas.edu'),
+('2001015', 'Ir. Nanda Putri', 'P', 'TE', 'nanda.p@universitas.edu'),
+('2001016', 'Dr. Omar Bakri', 'L', 'SI', 'omar.bakri@universitas.edu'),
+('2001017', 'Prof. Putri Kinanti', 'P', 'TI', 'putri.k@universitas.edu'),
+('2001018', 'Dr. Qasim Alif', 'L', 'TE', 'qasim.alif@universitas.edu'),
+('2001019', 'Ir. Rina Santi', 'P', 'SI', 'rina.santi@universitas.edu'),
+('2001020', 'Prof. Surya Jaya', 'L', 'TI', 'surya.jaya@universitas.edu'),
+('2001021', 'Dr. Tira Dewi', 'P', 'TE', 'tira.dewi@universitas.edu'),
+('2001022', 'Ir. Umar Hadi', 'L', 'SI', 'umar.hadi@universitas.edu'),
+('2001023', 'Prof. Vina Larasati', 'P', 'TI', 'vina.l@universitas.edu');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
   `nim` char(10) NOT NULL,
-  `nama` varchar(25) NOT NULL,
-  `gender` enum('l','p') NOT NULL,
-  `prodi` enum('ti','te','si') NOT NULL,
-  `alamat` text NOT NULL
+  `nama` varchar(25) DEFAULT NULL,
+  `gender` enum('L','P') DEFAULT NULL,
+  `prodi` enum('TI','TE','SI') DEFAULT NULL,
+  `alamat` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `mahasiswa`
+-- Dumping data untuk tabel `mahasiswa`
 --
 
 INSERT INTO `mahasiswa` (`nim`, `nama`, `gender`, `prodi`, `alamat`) VALUES
@@ -56,7 +87,13 @@ INSERT INTO `mahasiswa` (`nim`, `nama`, `gender`, `prodi`, `alamat`) VALUES
 --
 
 --
--- Indexes for table `mahasiswa`
+-- Indeks untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`nidn`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`nim`);
